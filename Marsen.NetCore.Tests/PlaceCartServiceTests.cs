2533456a280519ba6a70a3d7758abb135e8e3cbb
Marsen <thisismysoul@gmail.com>
2021-03-10 17:45:07 +0800

    DTO Sample

---
+++ Marsen.NetCore.Tests/PlaceCartServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Marsen.NetCore.Api.Tests
+{
+    public class PlaceCartServiceTests
+    {
+        readonly PlaceCartService placeCartService = new();
+
+        [Fact]
+        public void TestPutIn()
+        {
+            var product = new ProductDTO {Name = "Milk", Price = 10, Qty = 1};
+            placeCartService.PutIn(product);
+        }
+    }
+
+    public class PlaceCartService
+    {
+        public void PutIn(ProductDTO product)
+        {
+            this.calcTotal(product);
+        }
+
+        private void calcTotal(ProductDTO product)
+        {
+            //// Do Something
+        }
+    }
+
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public int Price { get; set; }
+    }
+}
