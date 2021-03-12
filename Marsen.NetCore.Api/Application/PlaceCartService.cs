a1a45251417d68cc2ebefa2ab93a18dc994c0f85
Marsen <thisismysoul@gmail.com>
2021-03-12 16:48:22 +0800

    Add GetCart

--- Marsen.NetCore.Api/Application/PlaceCartService.cs
+++ Marsen.NetCore.Api/Application/PlaceCartService.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Marsen.NetCore.Api.Model;
 
 namespace Marsen.NetCore.Api.Application
@@ -13,6 +14,11 @@ namespace Marsen.NetCore.Api.Application
         private void _cal(CartDto cart)
         {
             cart.Total = cart.LineItemList.Sum(x => x.SubTotal);
+        }
+
+        public CartDto GetCart()
+        {
+            throw new NotImplementedException("TODO get current user cart, so you need stateful and data persistence");
         }
     }
 }
