06baad8a101c739c44a59cabc9b28af45c7a1279
Marsen <thisismysoul@gmail.com>
2021-03-12 16:52:07 +0800

    add todo

--- Marsen.NetCore.Api/Application/PlaceCartService.cs
+++ Marsen.NetCore.Api/Application/PlaceCartService.cs
@@ -18,6 +18,7 @@ namespace Marsen.NetCore.Api.Application
 
         public CartDto GetCart()
         {
+            /// TODO: Implement
             throw new NotImplementedException("TODO get current user cart, so you need stateful and data persistence");
         }
     }
