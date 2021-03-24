209fba100b89d927673bf791992de1ae389ffc4e
Marsen <thisismysoul@gmail.com>
2021-03-24 19:13:41 +0800

    add gitignore

--- Marsen.NetCore.Api.Tests/UnitTest1.cs
+++ Marsen.NetCore.Api.Tests/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace TestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void METHOD()
         {
-
+            throw new NotImplementedException();
         }
     }
 }
