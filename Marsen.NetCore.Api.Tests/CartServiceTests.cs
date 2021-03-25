17a85284ba4d71fca962dba2126af8b5f78f118a
Marsen <thisismysoul@gmail.com>
2021-03-25 11:21:05 +0800

    add test SubTotal

--- Marsen.NetCore.Api.Tests/CartServiceTests.cs
+++ Marsen.NetCore.Api.Tests/CartServiceTests.cs
@@ -17,16 +17,25 @@ namespace Marsen.NetCore.Api.Tests
             this._cart.PutIn(milk);
             this._cart.GetCart().LineItems.Should().Contain(milk);
         }
+
+        [Fact]
+        public void TestSubTotal()
+        {
+            this._cart.PutIn(milk);
+            var item = this._cart.GetCart().LineItems.First();
+            Assert.Equal(10,item.Subtotal);
+        }
     }
 
     public class LineItem
     {
         public string Name { get; set; }
+        public int Subtotal { get; set; }
     }
 
     public class CartService
     {
-        readonly CartDTO _dto = new CartDTO {LineItems = new List<LineItem>()};
+        readonly CartDTO _dto = new() {LineItems = new List<LineItem>()};
 
         public void PutIn(LineItem lineItem)
         {
