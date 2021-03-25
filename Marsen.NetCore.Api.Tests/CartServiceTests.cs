fec1ff9a45160884f502292e1587d2596b80606f
Marsen <thisismysoul@gmail.com>
2021-03-25 10:21:01 +0800

    add test PutIn

--- Marsen.NetCore.Api.Tests/CartServiceTests.cs
+++ Marsen.NetCore.Api.Tests/CartServiceTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Marsen.NetCore.Api.Tests
@@ -6,9 +8,36 @@ namespace Marsen.NetCore.Api.Tests
     public class CartServiceTests
     {
         [Fact]
-        public void TestSubTotal()
+        public void TestPutIn()
+        {
+            CartService cartService = new CartService();
+            cartService.PutIn(new LineItem {Name = "Milk"});
+            var cart = cartService.GetCart();
+            var actual = cart.LineItems.Contains(new LineItem {Name = "Milk"});
+            Assert.Equal(true, actual);
+        }
+    }
+
+    public class LineItem
+    {
+        public string Name { get; set; }
+    }
+
+    public class CartService
+    {
+        public void PutIn(LineItem lineItem)
         {
             throw new NotImplementedException();
         }
+
+        public CartDTO GetCart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CartDTO
+    {
+        public IEnumerable<LineItem> LineItems { get; set; }
     }
 }
